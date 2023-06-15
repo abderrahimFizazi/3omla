d8815143cbebc56733d88038ed285a0f6ce34566
Oussama Hichou <usama.hicho.mail@gmail.com>
2023-06-15 12:35:40 +0100

    update

--- api/main.py
+++ api/main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
-from . import models
-from .database import engine
-from .routers import user, subscription, service, user_subscription, payment_status, notification
+from .routers import router
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
-
-app.include_router(user.router)
-app.include_router(subscription.router)
-app.include_router(service.router)
-app.include_router(user_subscription.router)
-app.include_router(payment_status.router)
-app.include_router(notification.router)
+app.include_router(router)
